d49b21956e77d2d33542ae7ebc70ae3e422c59a7
josephdlawson21 <josephdlawson21@gmail.com>
2018-01-29 17:09:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(x, y) {
+  return x.filter(function(num) {
+    return num.toLowerCase() === y.toLowerCase();
+  });
+}
+
+function fuzzyMatch(x, y) {
+  return x.filter(function(name) {
+    return name.slice(0, 2) === y;
+  });
+}
+
+function matchName(x, y) {
+  return x.filter(function(name) {
+    return name["name"] === y;
+  });
+}
